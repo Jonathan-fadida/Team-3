bda2f041bbce544a8107756e848c9d47641d168a
o1egkl <o1egkl@gmail.com>
2023-09-02 21:01:44 +0300

    Update secGroup.sh

--- utils/secGroup.sh
+++ utils/secGroup.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
 # Variables
-security_group_id="sg-12345678"  # Replace with your security group ID
-description="SSH Rule"
+security_group_name="MySecurityGroup"  # Replace with your desired security group name
+description="My Security Group for SSH"
 port=22
 cidr_ip="0.0.0.0/0"  # Adjust to your desired IP range
 
-# Create a security group ingress rule for SSH (port 22)
+# Create a new security group
+security_group_id=$(aws ec2 create-security-group \
+    --group-name "$security_group_name" \
+    --description "$description" \
+    --vpc-id "your-vpc-id"  # Replace with your VPC ID \
+    --output text)
+
+# Add an ingress rule to allow SSH traffic (port 22)
 aws ec2 authorize-security-group-ingress \
     --group-id $security_group_id \
     --protocol tcp \
     --port $port \
     --cidr $cidr_ip \
-    --description "$description"
+    --description "SSH Rule"
 
-# Output success message
-echo "Security group rule for port $port (SSH) has been created."
+# Output the created security group ID
+echo "Security group $security_group_name (ID: $security_group_id) has been created with SSH rule."
